545ab198188155228ec379f99dc0cfbd4ea118ce
rinick <rinick@gmail.com>
2022-12-24 02:34:26 -0800

    fix the build

--- 5.1/src/index.tsx
+++ 5.1/src/index.tsx
@@ -3,4 +3,38 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Demo from './demo';
 
-createRoot(document.getElementById('container')).render(<Demo />);
+createRoot(document.getElementById('container')).render(
+<div>
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+  <Demo />
+</div>
+);
+
